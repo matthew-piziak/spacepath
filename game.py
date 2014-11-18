dc7f9b6b5c04cb169bb90c2bdf8e734fc54c1315
Matthew Piziak <matthew.piziak@gmail.com>
2014-11-18 15:28:33 -0800

    cleaned up game

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ import math
 import time
 import subprocess
 import os
+from collections import namedtuple
 
 # drawing constants
 NODE_RADIUS = 12
@@ -25,18 +26,13 @@ NUM_OBSTACLES = random.randint(2, 8)
 START = newt.NewtNode(0, 0, 0, 0, 0)
 GOAL = newt.NewtNode(random.randint(70, 200), random.randint(70, 150), 0, 0, 0)
 
-class Obstacle(object):
-    """an obstacle for the path to avoid"""
-    def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
-        self.radius = radius
+Obstacle = namedtuple('Obstacle', ['x', 'y', 'radius'])
 
-    def draw(self, window):
-        """draw obstacle"""
-        location = (self.x * DRAW_SCALE, self.y * DRAW_SCALE)
-        radius = (self.radius * DRAW_SCALE) - NODE_RADIUS
-        pygame.draw.circle(window, OBSTACLE_COLOR, location, radius)
+def draw_obstacle(window, obstacle):
+    """draw obstacle"""
+    location = (obstacle.x * DRAW_SCALE, obstacle.y * DRAW_SCALE)
+    radius = (obstacle.radius * DRAW_SCALE) - NODE_RADIUS
+    pygame.draw.circle(window, OBSTACLE_COLOR, location, radius)
 
 def generate_random_obstacle():
     """generate random obstacle"""
@@ -66,11 +62,13 @@ def draw_node(window, node):
     pygame.draw.line(window, NODE_COLOR, (node_x, node_y), (angle_x, angle_y))
 
 def draw_scene(window, obstacles):
+    """draw scene"""
     window.fill(BACKGROUND_COLOR)
     for obstacle in obstacles:
-        obstacle.draw(window)
-    
+        draw_obstacle(window, obstacle)
+
 def main():
+    """break this out"""
     obstacles = []
     for _ in range(NUM_OBSTACLES):
         obstacle = generate_random_obstacle()
@@ -79,23 +77,23 @@ def main():
     draw_scene(window, obstacles)
     draw_goal(window)
     pygame.display.flip()
-    PATH = pathing.a_star(START,
+    path = pathing.a_star(START,
                           GOAL,
                           newt.adjacent,
                           lambda n, g: newt.heuristic(n, g, obstacles),
                           newt.success)
-    if len(PATH) == 1:
+    if len(path) == 1:
         draw_scene(window, obstacles)
-        draw_node(window, PATH[0])
-        for adj in newt.adjacent(PATH[0]):
+        draw_node(window, path[0])
+        for adj in newt.adjacent(path[0]):
             draw_node(window, adj)
         pygame.display.flip()
         time.sleep(3)
         exit()
     filelist = [f for f in os.listdir(".") if f.endswith(".png")]
-    for f in filelist:
-        os.remove(f)
-    for screen, node in enumerate(PATH):
+    for filename in filelist:
+        os.remove(filename)
+    for screen, node in enumerate(path):
         draw_scene(window, obstacles)
         draw_goal(window)
         draw_node(window, node)
@@ -104,7 +102,7 @@ def main():
         pygame.image.save(window, str(screen).zfill(4) + "screen.png")
     print("label: " + str(int(time.time())))
     gif_command = "bash make_gif.sh maneuver" + str(int(time.time())) + ".gif"
-    process = subprocess.Popen(gif_command.split(), stdout=subprocess.PIPE)
+    subprocess.Popen(gif_command.split(), stdout=subprocess.PIPE)
 
 if __name__ == "__main__":
     while True:
